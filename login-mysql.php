25ec5f065971a7fe32f61b98093d8a1dd33e4a41
Gerard de Jong <gerard@dejong.za.net>
2017-07-27 15:23:33 +0200

    Now supports logon via mysql

---
+++ login-mysql.php
@@ -0,0 +1,21 @@
+<?php
+require_once("config.php");
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+mysql_select_db(DB_NAME, $link);
+
+$query = 'SELECT COUNT(*) FROM user WHERE email = \'' . $_POST['email'] . '\' AND password = \'' . $_POST['password'] . '\';';
+
+$result = mysql_query($query);
+$value = mysql_result($result, 0);
+
+if($value) {
+    header('Location: portal.html');
+} else {
+    header('Location: forbidden.html');
+}
+
+file_put_contents('query.log', $query . PHP_EOL, FILE_APPEND); 
+
+mysql_free_result($result);
+mysql_close($link);
